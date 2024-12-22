568589a96aa10e0a6308ab149b2cbca57998e73d
Damian <damianperezherediaalejandro@gmail.com>
2024-12-22 20:11:55 -0300

    Finalizima

--- static/JS/Input.js
+++ static/JS/Input.js
@@ -16,3 +16,22 @@ document.addEventListener("DOMContentLoaded", function () {
         fileNameEnviar.style.display = 'inline';
     });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const fileInputExcel = document.getElementById('archivo');
+    const fileNameExcel = document.getElementById('file-name');
+    const fileInputEnviar = document.getElementById('imagen');
+    const fileNameEnviar = document.getElementById('file-name-enviar');  // Asegúrate de que este ID coincide con el HTML
+
+    fileInputExcel.addEventListener('change', function () {
+        const fileName = this.files[0]?.name || "Ningún archivo seleccionado";
+        fileNameExcel.textContent = fileName;
+        fileNameExcel.style.display = 'inline';
+    });
+
+    fileInputEnviar.addEventListener('change', function () {
+        const fileName = this.files[0]?.name || "Ningún archivo seleccionado";
+        fileNameEnviar.textContent = fileName;
+        fileNameEnviar.style.display = 'inline';  // Este código ya está bien
+    });
+});
+
